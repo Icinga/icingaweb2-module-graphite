3a0d5e4c7941fc478828b83030b05c21431a1a32
Thomas Gelf <thomas@gelf.net>
2015-06-22 16:25:44 +0200

    GraphDatasource: add CSS color helpers

--- library/Graphite/GraphDatasource.php
+++ library/Graphite/GraphDatasource.php
@@ -6,6 +6,30 @@ use Icinga\Web\Url;
 
 class GraphDatasource
 {
+    protected static $cssColors = array(
+        'black'     => 'rgb(0,0,0)',
+        'white'     => 'rgb(255,255,255)',
+        'blue'      => 'rgb(100,100,255)',
+        'green'     => 'rgb(0,200,0)',
+        'red'       => 'rgb(200,0,50)',
+        'yellow'    => 'rgb(255,255,0)',
+        'orange'    => 'rgb(255, 165, 0)',
+        'purple'    => 'rgb(200,100,255)',
+        'brown'     => 'rgb(150,100,50)',
+        'aqua'      => 'rgb(0,150,150)',
+        'gray'      => 'rgb(175,175,175)',
+        'grey'      => 'rgb(175,175,175)',
+        'magenta'   => 'rgb(255,0,255)',
+        'pink'      => 'rgb(255,100,100)',
+        'gold'      => 'rgb(200,200,0)',
+        'rose'      => 'rgb(200,150,200)',
+        'darkblue'  => 'rgb(0,0,255)',
+        'darkgreen' => 'rgb(0,255,0)',
+        'darkred'   => 'rgb(255,0,0)',
+        'darkgray'  => 'rgb(111,111,111)',
+        'darkgrey'  => 'rgb(111,111,111)'
+    );
+
     protected $path;
 
     protected $color;
@@ -59,6 +83,21 @@ class GraphDatasource
             return $this->alias;
         }
         return $this->path;
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    public function getColorCss()
+    {
+        $color = $this->getColor();
+        if (array_key_exists($color, self::$cssColors)) {
+            return self::$cssColors[$color];
+        }
+
+        return $color;
     }
 
     protected function func()
