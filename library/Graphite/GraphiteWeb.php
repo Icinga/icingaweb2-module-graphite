47237c7cb4b9d414b4a9da481e252124e4e5b70f
Thomas Gelf <thomas@gelf.net>
2015-06-25 15:30:42 +0200

    GraphiteWeb: comments

--- library/Graphite/GraphiteWeb.php
+++ library/Graphite/GraphiteWeb.php
@@ -5,25 +5,55 @@ namespace Icinga\Module\Graphite;
 use Icinga\Module\Graphite\GraphTemplate;
 use Icinga\Module\Graphite\GraphiteQuery;
 
+/**
+ * Graphite web app
+ *
+ * Simple class, start point for all your queries
+ */
 class GraphiteWeb
 {
+    /**
+     * Graphite webapp base url
+     *
+     * @var string
+     */
     protected $baseUrl;
 
+    /**
+     * Construct a new graphite webapp instance
+     *
+     * @param $baseUrl string Graphite webapp base url
+     */
     public function __construct($baseUrl)
     {
         $this->baseUrl = $baseUrl;
     }
 
+    /**
+     * Initiate a new query object
+     *
+     * @return GraphiteQuery
+     */
     public function select()
     {
         return new GraphiteQuery($this);
     }
 
+    /**
+     * Retrieve out base url
+     *
+     * @return string
+     */
     public function getBaseUrl()
     {
         return $this->baseUrl;
     }
 
+    /**
+     * Retrieve a list of metrics fitting the given filter
+     *
+     * @return array
+     */
     public function listMetrics($filter)
     {
         $res = json_decode(
