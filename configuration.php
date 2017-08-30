f86bf88dcc1bead3836b298eadb4c3db8a89bd9f
Alexander A. Klimov <alexander.klimov@netways.de>
2017-08-30 14:55:36 +0200

    Make the Graphite Web URL configurable over the WebUI
    refs #28

--- configuration.php
+++ configuration.php
@@ -4,3 +4,9 @@
 
 $this->menuSection(N_('Graphite'), ['icon' => 'chart-area'])->setUrl('graphite/show/overview');
 
+$this->provideConfigTab('backend', array(
+    'title' => $this->translate('Configure the Graphite Web backend'),
+    'label' => $this->translate('Backend'),
+    'url' => 'config'
+));
+
