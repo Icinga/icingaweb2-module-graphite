a72f923de1163194058106a8f436f8a06caf8e8f
Alexander A. Klimov <alexander.klimov@netways.de>
2017-09-01 18:08:33 +0200

    Remove obsolete monitoring hooks
    refs #27

--- run.php
+++ run.php
@@ -1,6 +1,4 @@
 <?php
 
 /** @var \Icinga\Application\Modules\Module $this */
-$this->provideHook('monitoring/HostActions');
-$this->provideHook('monitoring/ServiceActions');
 $this->provideHook('monitoring/DetailviewExtension');
