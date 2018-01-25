bc6adceb135f44b10727c06c1b39bc330768e911
Alexander A. Klimov <alexander.klimov@netways.de>
2018-01-25 18:30:22 +0100

    Fix broken graphs for monitored objects with obscured check commands

--- application/controllers/GraphController.php
+++ application/controllers/GraphController.php
@@ -7,6 +7,7 @@ use Icinga\Exception\Http\HttpBadRequestException;
 use Icinga\Exception\Http\HttpNotFoundException;
 use Icinga\Module\Graphite\Graphing\GraphingTrait;
 use Icinga\Module\Graphite\Web\Controller\MonitoringAwareController;
+use Icinga\Module\Graphite\Web\Widget\Graphs;
 use Icinga\Util\TimezoneDetect;
 use Icinga\Web\UrlParams;
 
@@ -48,8 +49,9 @@ class GraphController extends MonitoringAwareController
 
     public function hostAction()
     {
+        $checkCommandColumn = '_host_' . Graphs::getObscuredCheckCommandCustomVar();
         $host = $this->applyMonitoringRestriction(
-            $this->backend->select()->from('hoststatus', ['host_check_command'])
+            $this->backend->select()->from('hoststatus', ['host_check_command', $checkCommandColumn])
         )
             ->where('host_name', $this->filterParams->getRequired('host.name'))
             ->limit(1) // just to be sure to save a few CPU cycles
@@ -59,13 +61,14 @@ class GraphController extends MonitoringAwareController
             throw new HttpNotFoundException('%s', $this->translate('No such host'));
         }
 
-        $this->supplyImage($host->host_check_command);
+        $this->supplyImage($host->host_check_command, $host->$checkCommandColumn);
     }
 
     public function serviceAction()
     {
+        $checkCommandColumn = '_service_' . Graphs::getObscuredCheckCommandCustomVar();
         $service = $this->applyMonitoringRestriction(
-            $this->backend->select()->from('servicestatus', ['service_check_command'])
+            $this->backend->select()->from('servicestatus', ['service_check_command', $checkCommandColumn])
         )
             ->where('host_name', $this->filterParams->getRequired('host.name'))
             ->where('service_description', $this->filterParams->getRequired('service.name'))
@@ -76,17 +79,22 @@ class GraphController extends MonitoringAwareController
             throw new HttpNotFoundException('%s', $this->translate('No such service'));
         }
 
-        $this->supplyImage($service->service_check_command);
+        $this->supplyImage($service->service_check_command, $service->$checkCommandColumn);
     }
 
     /**
      * Do all monitored object type independend actions
      *
-     * @param   string  $checkCommand   The check command of the monitored object we supply an image for
+     * @param   string      $checkCommand           The check command of the monitored object we supply an image for
+     * @param   string|null $obscuredCheckCommand   The "real" check command (if any) of the monitored object
+     *                                              we display graphs for
      */
-    protected function supplyImage($checkCommand)
+    protected function supplyImage($checkCommand, $obscuredCheckCommand)
     {
-        $templates = $this->getAllTemplates()->getTemplates($checkCommand);
+        $templates = $this->getAllTemplates()->getTemplates(
+            $obscuredCheckCommand === null ? $checkCommand : $obscuredCheckCommand
+        );
+
         if (! isset($templates[$this->graphParams['template']])) {
             throw new HttpNotFoundException($this->translate('No such template'));
         }
