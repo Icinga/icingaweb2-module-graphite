e9e46d2d71bb6e2881206939b29548ee811b2a90
Eric Lippmann <eric.lippmann@icinga.com>
2021-02-22 14:06:02 +0100

    Menu: Fix limits

--- configuration.php
+++ configuration.php
@@ -4,8 +4,8 @@
 
 /** @var \Icinga\Application\Modules\MenuItemContainer $section */
 $section = $this->menuSection(N_('Graphite'), ['icon' => 'chart-area']);
-$section->add(N_('Hosts'), ['url' => 'graphite/list/hosts?graphs_limit=2']);
-$section->add(N_('Services'), ['url' => 'graphite/list/services?graphs_limit=2']);
+$section->add(N_('Hosts'), ['url' => 'graphite/list/hosts?limit=2']);
+$section->add(N_('Services'), ['url' => 'graphite/list/services?limit=2']);
 
 $this->provideConfigTab('backend', array(
     'title' => $this->translate('Configure the Graphite Web backend'),
