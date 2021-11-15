68c572edb4cc6d4fdcbf34482032a5accc04cbd6
Sukhwinder Dhillon <sukhwinder33445@gmail.com>
2021-11-15 17:08:29 +0100

    Add `IcingadbSupport` hook class

---
+++ library/Graphite/ProvidedHook/Icingadb/IcingadbSupport.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Icinga\Module\Graphite\ProvidedHook\Icingadb;
+
+use Icinga\Module\Icingadb\Hook\IcingadbSupportHook;
+
+class IcingadbSupport extends IcingadbSupportHook
+{
+
+}
--- run.php
+++ run.php
@@ -6,3 +6,4 @@ require_once $this->getLibDir() . '/vendor/Psr/Loader.php';
 require_once $this->getLibDir() . '/vendor/iplx/Loader.php';
 
 $this->provideHook('monitoring/DetailviewExtension');
+$this->provideHook('icingadb/IcingadbSupport');
