be0e39b2c96a8a33b307817cfe6975052bacfd8b
Sukhwinder Dhillon <sukhwinder33445@gmail.com>
2022-02-17 11:04:50 +0100

    configuration.php: Add menu section routes for icingadb backend

--- configuration.php
+++ configuration.php
@@ -3,9 +3,18 @@
 /** @var \Icinga\Application\Modules\Module $this */
 
 /** @var \Icinga\Application\Modules\MenuItemContainer $section */
+
+use Icinga\Module\Graphite\ProvidedHook\Icingadb\IcingadbSupport;
+
 $section = $this->menuSection(N_('Graphite'), ['icon' => 'chart-area']);
-$section->add(N_('Hosts'), ['url' => 'graphite/list/hosts?graphs_limit=2']);
-$section->add(N_('Services'), ['url' => 'graphite/list/services?graphs_limit=2']);
+
+if ($this::exists('icingadb') && IcingadbSupport::useIcingaDbAsBackend()) {
+    $section->add(N_('Hosts'), ['url' => 'graphite/hosts?graphs_limit=2']);
+    $section->add(N_('Services'), ['url' => 'graphite/services?graphs_limit=2']);
+} else {
+    $section->add(N_('Hosts'), ['url' => 'graphite/list/hosts?graphs_limit=2']);
+    $section->add(N_('Services'), ['url' => 'graphite/list/services?graphs_limit=2']);
+}
 
 $this->provideConfigTab('backend', array(
     'title' => $this->translate('Configure the Graphite Web backend'),
