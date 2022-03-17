db1ddccb78050e2836f199d1ecbe77c8ca9ff8b8
Sukhwinder Dhillon <sukhwinder.dhillon@icinga.com>
2022-03-17 11:12:07 +0100

    IcingadbGraphs.php: Use BaseFilter and remove unused properties

--- library/Graphite/Web/Widget/IcingadbGraphs.php
+++ library/Graphite/Web/Widget/IcingadbGraphs.php
@@ -9,18 +9,18 @@ use Icinga\Module\Graphite\Web\Widget\Graphs\Icingadb\IcingadbHost;
 use Icinga\Module\Graphite\Web\Widget\Graphs\Icingadb\IcingadbService;
 use Icinga\Module\Icingadb\Common\Links;
 use Icinga\Module\Icingadb\Widget\EmptyState;
-use Icinga\Web\Url;
 use Icinga\Module\Icingadb\Model\Host;
-use InvalidArgumentException;
 use ipl\Html\BaseHtmlElement;
 use ipl\Html\Html;
+use ipl\Html\HtmlDocument;
 use ipl\Html\HtmlString;
+use ipl\Orm\ResultSet;
 use ipl\Stdlib\BaseFilter;
 use ipl\Web\Filter\QueryString;
 use ipl\Web\Widget\Link;
 
 /**
-* Base class for list items
+* Class for creating graphs of icingadb objects
 */
 class IcingadbGraphs extends BaseHtmlElement
 {
@@ -33,30 +33,14 @@ class IcingadbGraphs extends BaseHtmlElement
 
     protected $tag = 'div';
 
-    /** @var Url */
-    protected $hostBaseUrl;
-
-    /** @var Url */
-    protected $serviceBaseUrl;
-
-    /** Create a new Graph item
+    /**
+     * Create a new Graph item
      *
-     * @param Iterable $objects
-     *
-     * @param Url $hostBaseUrl Base url for hosts
-     *
-     * @param Url|null $serviceBaseUrl Base url for services
-     *
+     * @param ResultSet $objects
      */
-    public function __construct($objects)
+    public function __construct(ResultSet $objects)
     {
-        if (! is_iterable($objects)) {
-            throw new InvalidArgumentException('Data must be an array or an instance of Traversable');
-        }
-
         $this->objects = $objects;
-      /*  $this->hostBaseUrl = $hostBaseUrl;
-        $this->serviceBaseUrl = $serviceBaseUrl;*/
     }
 
     protected function assemble()
@@ -68,6 +52,10 @@ class IcingadbGraphs extends BaseHtmlElement
         foreach ($this->objects as $object) {
             $this->add($this->createGridItem($object));
         }
+
+        $document = new HtmlDocument();
+        $document->addHtml(Html::tag('div', ['class' => 'graphite-graph-color-registry']), $this);
+        $this->prependWrapper($document);
     }
 
     protected function createGridItem($object)
@@ -80,31 +68,39 @@ class IcingadbGraphs extends BaseHtmlElement
             $hostObj = $object->host;
         }
 
-        $gridItem = Html::tag('div', ['class' => 'grid-item']);
-        $header = Html::tag('h2');
+        $hostUrl = Links::host($hostObj);
+
+        if ($this->hasBaseFilter()) {
+            $hostUrl->addFilter(Filter::fromQueryString(QueryString::render($this->getBaseFilter())));
+        }
 
         $hostLink =  new Link(
             $graph->createHostTitle(),
-            Links::host($hostObj)
-                ->addFilter(
-                    Filter::fromQueryString(QueryString::render($this->getBaseFilter()))
-                ),
+            $hostUrl,
             ['data-base-target' => '_next']
         );
 
-        $serviceLink = $graph->getObjectType() === 'service'
-            ? new Link(
+        $serviceLink = null;
+        if ($graph->getObjectType() === 'service') {
+            $serviceUrl = Links::service($object, $hostObj);
+
+            if ($this->hasBaseFilter()) {
+                $serviceUrl->addFilter(Filter::fromQueryString(QueryString::render($this->getBaseFilter())));
+            }
+
+            $serviceLink = new Link(
                 $graph->createServiceTitle(),
-                Links::service($object, $hostObj)->addFilter(
-                    Filter::fromQueryString(QueryString::render($this->getBaseFilter()))
-                ),
+                $serviceUrl,
                 ['data-base-target' => '_next']
-            )
-            : null;
+            );
+        }
+
+        $gridItem = Html::tag('div', ['class' => 'grid-item']);
+        $header = Html::tag('h2');
 
         $header->add([$hostLink, $serviceLink]);
         $gridItem->add($header);
 
-        return $gridItem->add(HtmlString::create($graph->handleRequest()));
+        return $gridItem->add(HtmlString::create($graph->setPreloadDummy()->handleRequest()));
     }
 }
